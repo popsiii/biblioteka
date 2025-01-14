cd0b0143095fc5687789644b71cd094451c663c3
Weronika <wmarszalek@st.swps.edu.pl>
2025-01-14 15:33:11 +0100

    pierwszy komit to jest tragedia

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,19 @@
+from datetime import date
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+PLEC = models.IntegerChoices('Płeć', 'Kobieta Męzczyzna Inna Nie_chcę_podawać')
+
+class Uzytkownik(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='uzytkownik')
+    imie = models.CharField(max_length=60)
+    nazwisko = models.CharField(max_length=80)
+    email = models.EmailField(unique=True)
+    plec = models.IntegerField(choices=PLEC.choices, default=PLEC.choices, default = "-" )
+    data_dodania = models.DateField(default=date.today, blank=False, null=False)
+
+
+    
+
+
+
