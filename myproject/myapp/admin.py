3f731ae33b257977025c2838add0ccefce372d83
Weronika <wmarszalek@st.swps.edu.pl>
2025-01-20 16:43:47 +0100

    dodanie modelu ksiązka i modelu gatunek

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import Uzytkownik
+from .models import Uzytkownik, Ksiazka, Gatunek
 admin.site.register(Uzytkownik)
+admin.site.register(Ksiazka)
+admin.site.register(Gatunek)
 
 admin.register(Uzytkownik)
 class UzytkownikAdmin(admin.ModelAdmin):
     list_display = ('imie', 'nazwisko', 'email', 'plec', 'data_dodania')
+
