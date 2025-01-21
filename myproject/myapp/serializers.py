31b6cf79c82ce6c9cc72082ffed60aeb00ee9db2
Weronika <wmarszalek@st.swps.edu.pl>
2025-01-21 16:30:43 +0100

    kolejne zmiany na serializery i urlsy

--- myproject/myapp/serializers.py
+++ myproject/myapp/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Ksiazka
 
-class BookSerializer(serializers.ModelSerializer):
+class KsiazkaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ksiazka
         fields = ['tytul', 'autor', 'wydawnictwo', 'rok_wydania', 'liczba_stron', 'ISBN', 'okładka', 'gatunek']
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Ksiazka
+from .serializers import KsiazkaSerializer
 
-# Create your views here.
+class KsiazkaListCreate(generics.ListCreateAPIView):
+    queryset = Ksiazka.objects.all()
+    serializer_class = KsiazkaSerializer
+
+class KsiazkaDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ksiazka.objects.all()
+    serializer_class = KsiazkaSerializer
