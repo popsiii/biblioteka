f597f326d19828f87b3e7d4b0eed65b37c95bc1c
Marta <mpopielska1@st.swp0s.edu.pl>
2025-01-28 03:17:19 +0100

    jest21costam

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -26,3 +28,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')), 
     path('', include('myapp.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
